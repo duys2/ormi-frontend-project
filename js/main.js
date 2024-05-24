5f797a791102ba91c9679212b56fbb8a0c3f0136
Ays <minigun5@naver.com>
2024-05-24 17:48:31 +0900

    [Add] 카카오 맵 api 적용

--- js/main.js
+++ js/main.js
@@ -1,3 +1,47 @@
+/* ---------------- 추가 이미지 랜더링 영역 ---------------- */
+
+const listPic = document.querySelector('.list-pic');
+const btn = document.querySelector('.more-btn');
+let pageToPatch = 1;
+
+btn.addEventListener('click', () => { fetchImages(pageToPatch += 1) }); // 그래서 이 익명 함수가 실행되고 그 뒤에 fetchImages()가 실행됨
+
+async function fetchImages(page) {
+    try {
+        const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=5`); // 계속 다른 사진이 나오게 하려면 page=1의 값을 1이 아닌 2, 3, 4 이렇게 올려줘야 함
+
+        if (!response.ok) {
+            throw new Error('네트워크 응답에 문제가 있습니다.');
+        }
+
+        const datas = await response.json();
+        console.log(datas);
+        makeImageList(datas);
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function makeImageList(datas) {
+    datas.forEach((data) => {
+        listPic.insertAdjacentHTML('beforeend', `<li class="img-width"><img src="${data.download_url}" alt=""></li>`); // innerHTML() 보다는 이게 더 성능이 좋음
+    });
+}
+
+/* ---------------- 지도 구현 영역 ---------------- */
+
+const mapContainer = document.getElementById('map'), // 지도를 표시할 div
+    mapOption = {
+        center: new kakao.maps.LatLng(33.4422, 126.5715), // 지도의 중심좌표
+        level: 2 // 지도의 확대 레벨
+    };
+
+// 지도를 표시할 div와 지도 옵션으로 지도 생성
+const map = new kakao.maps.Map(mapContainer, mapOption);
+
+/* ---------------- 모달 랜더링 영역 ---------------- */
+
 // 모달 창을 열기 위한 요소 할당
 let open = document.querySelector(".modal-open");
 // 모달 창에 해당하는 요소 할당
@@ -16,12 +60,12 @@ open.onclick = function () {
 // 모달 창을 열기 위한 클릭 이벤트 등록
 open.addEventListener("click", function () {
     // 모달 창 요소에 active 클래스를 추가하여 보이게 하기
-    modalPage.classList.add("active");
+    modal.classList.add("active");
 });
 
 /* 위 함수를 화살표 함수로
 open.addEventListener("click", () => {
-    modalPage.classList.add("active");
+    modal.classList.add("active");
 });
 */
 
