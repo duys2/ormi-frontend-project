14f180110a198ab4b15895e4723e82ad24f3d2e2
Ays <minigun5@naver.com>
2024-05-25 10:01:45 +0900

    [Add] scroll top 버튼 수정, 지도 중심좌표 수정

--- js/main.js
+++ js/main.js
@@ -35,8 +35,8 @@ function makeImageList(datas) {
 
 const mapContainer = document.getElementById('map'), // 지도를 표시할 div
     mapOption = {
-        center: new kakao.maps.LatLng(33.4422, 126.5715), // 지도의 중심좌표
-        level: 2 // 지도의 확대 레벨
+        center: new kakao.maps.LatLng(37.5798, 126.9771), // 지도의 중심좌표
+        level: 3 // 지도의 확대 레벨
     };
 
 // 지도를 표시할 div와 지도 옵션으로 지도 생성
