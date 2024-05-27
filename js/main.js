0d8b3494f2eee069512f363666a8fbbe225ee385
Ays <minigun5@naver.com>
2024-05-27 17:42:27 +0900

    [Comment] 주석 추가 및 변경

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 /* ---------------- 다운로드 버튼 영역 ---------------- */
+
 const downloadBtns = document.querySelectorAll('.download-btn'); // 배열로 넘어옴
 
 // 각 버튼에 이벤트 리스너를 추가하여 버튼을 누르면 이미지를 다운로드
@@ -20,9 +21,9 @@ downloadBtns.forEach((button, index) => {
         const link = document.createElement('a');
         link.href = imageUrl;
         link.download = imageName; // 다운로드될 파일 이름 설정
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
+        document.body.appendChild(link); // a 추가
+        link.click(); // 이미지 다운
+        document.body.removeChild(link); // a 삭제
     });
 });
 
@@ -33,11 +34,11 @@ const moreBtn = document.querySelector('.more-btn');
 
 let pageToPatch = 3;
 
-moreBtn.addEventListener('click', () => { fetchImages(pageToPatch += 1) }); // 그래서 이 익명 함수가 실행되고 그 뒤에 fetchImages()가 실행됨
+moreBtn.addEventListener('click', () => { fetchImages(pageToPatch += 1) }); // 익명 함수 실행 후 fetchImages() 실행
 
 async function fetchImages(page) {
     try {
-        const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=12`); // 계속 다른 사진이 나오게 하려면 page=1의 값을 1이 아닌 2, 3, 4 이렇게 올려줘야 함
+        const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=12`); // 계속 다른 사진이 나오게 하기 위해 page=1의 2, 3, 4 등으로 증가
 
         if (!response.ok) {
             throw new Error('네트워크 응답에 문제가 있습니다.');
@@ -55,7 +56,7 @@ async function fetchImages(page) {
 function makeImageList(datas) {
 
     datas.forEach((data) => {
-        imgList.insertAdjacentHTML('beforeend', `<img src="${data.download_url}" alt="mid area img">`); // innerHTML() 보다는 이게 더 성능이 좋음
+        imgList.insertAdjacentHTML('beforeend', `<img src="${data.download_url}" alt="mid area img">`); // img 삽입
     });
 }
 
@@ -72,12 +73,9 @@ const map = new kakao.maps.Map(mapContainer, mapOption);
 
 /* ---------------- 모달 랜더링 영역 ---------------- */
 
-// 모달 창을 열기 위한 요소 할당
-const open = document.querySelector(".modal-open");
-// 모달 창에 해당하는 요소 할당
-const modal = document.querySelector(".modal");
-// 모달 창을 닫기 위한 요소 할당
-const close = document.querySelector(".close-modal");
+const open = document.querySelector(".modal-open"); // 모달 창을 열기 위한 요소 할당
+const modal = document.querySelector(".modal"); // 모달 창에 해당하는 요소 할당
+const close = document.querySelector(".close-modal"); // 모달 창을 닫기 위한 요소 할당
 
 /*
 open.onclick = function () { 
@@ -89,8 +87,7 @@ open.onclick = function () {
 
 // 모달 창을 열기 위한 클릭 이벤트 등록
 open.addEventListener("click", function () {
-    // 모달 창 요소에 active 클래스를 추가하여 보이게 하기
-    modal.classList.add("active");
+    modal.classList.add("active"); // 모달 창 요소에 active 클래스를 추가하여 보이게 하기
 });
 
 /* 위 함수를 화살표 함수로
@@ -101,6 +98,5 @@ open.addEventListener("click", () => {
 
 // 마찬가지로 모달 창을 닫기 위한 클릭 이벤트 등록
 close.onclick = function () {
-    // 모달 창 요소의 active 클래스를 제거해서 안 보이게 하기
-    modal.classList.remove("active");
+    modal.classList.remove("active"); // 모달 창 요소의 active 클래스를 제거해서 안 보이게 하기
 }
